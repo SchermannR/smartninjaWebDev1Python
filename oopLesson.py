9c9b824bda501e1f4152e8be9d41f1217d96df37
Raphael <raphael.schermann@gmx.at>
2019-03-14 18:44:26 +0100

    python oop stage 2

--- oopLesson.py
+++ oopLesson.py
@@ -12,6 +12,12 @@ class BasketballPlayer():
         self.assists = assists
 
 
-createfancybasketballplayer = BasketballPlayer("Very","Sexy",221, 100, 1000,1000,1000)
+createfancybasketballplayer = BasketballPlayer("Very","Sexy",221, 100,999,1000,888)
+#with the keyword you can change the order of the parametes
+lebron = BasketballPlayer(first_name="Lebron", last_name="James", weight_kg=113, height_cm=203, points=27.2, rebounds=7.4, assists=7.2)
+kev_dur = BasketballPlayer(points=27.2,first_name="Kevin", last_name="Durant", height_cm=210, weight_kg=108, rebounds=7.1, assists=4)
 print(createfancybasketballplayer.first_name)
 print(createfancybasketballplayer.last_name)
+print(lebron.weight_kg)
+print(createfancybasketballplayer.points)
+print(kev_dur.points)
