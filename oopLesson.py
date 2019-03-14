21de2250807c50609ead1d3e0565da8324ccbfd2
Raphael <raphael.schermann@gmx.at>
2019-03-14 18:58:15 +0100

    python oop stage 3

--- oopLesson.py
+++ oopLesson.py
@@ -1,6 +1,7 @@
 #create a class
 class BasketballPlayer():
     first_name = "abc"
+
     #initialize a new object / contructor
     def __init__(self, first_name, last_name, height_cm, weight_kg, points, rebounds, assists):
         self.first_name = first_name
@@ -10,6 +11,19 @@ class BasketballPlayer():
         self.points = points
         self.rebounds = rebounds
         self.assists = assists
+        self.__attr = 0 #private variable
+
+    def weight_to_lbs(self):
+        pounds = self.weight_kg * 2.20462262
+        return pounds
+
+    #common used "workaround" to give access to private attributes (if its needed)
+    def get_private_attribute(self): #public function
+        return self.__attr
+
+    def set_private_attribute(self,value): #public function
+        self.__attr = value
+
 
 
 createfancybasketballplayer = BasketballPlayer("Very","Sexy",221, 100,999,1000,888)
@@ -21,3 +35,25 @@ print(createfancybasketballplayer.last_name)
 print(lebron.weight_kg)
 print(createfancybasketballplayer.points)
 print(kev_dur.points)
+
+#create a list of our objects (players)
+list_bball_players = [lebron, kev_dur]
+print(type(list_bball_players))
+#print the last and the firstname of our players
+for player in list_bball_players:
+    print(player.last_name + ", " + player.first_name)
+
+print(lebron.weight_to_lbs())
+
+"""
+public private access example
+"""
+
+lebron.first_name="modified lebron"
+print(lebron.first_name)
+#private attribute access
+#print(lebron.__attribute) --> will not work
+print(lebron.get_private_attribute())
+#lebron.__attribute = 10 --> will not work
+lebron.set_private_attribute(10)
+print(lebron.get_private_attribute())
